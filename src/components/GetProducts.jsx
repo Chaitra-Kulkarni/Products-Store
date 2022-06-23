877906481320c9c7767d9bb319a7d6b569a8dc87
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-06-23 18:46:03 +0530

    add category buttons to filter products

--- src/components/GetProducts.jsx
+++ src/components/GetProducts.jsx
@@ -19,13 +19,43 @@ const GetProducts = () => {
       >
         View Products
       </button>
+      <div>
+        <label>Filter :</label>
+        <a className="btn btn-warning btn-sm m-2" href="#" value="electronics">
+          Electronics
+        </a>
+        <a className="btn btn-warning btn-sm m-2" href="#" value="jewelery">
+          Jewelery
+        </a>
+        <a
+          className="btn btn-warning btn-sm m-2"
+          href="#"
+          value="men's clothing"
+        >
+          Men's clothing
+        </a>
+        <a
+          className="btn btn-warning btn-sm m-2"
+          href="#"
+          value="women's clothing"
+        >
+          Women's clothing
+        </a>
+      </div>
       <div className="container-fluid">
         <div className="row">
           {productsList.map((product) => {
             return (
-              <div className="col-9 border border-dark m-1" style={{width: "18rem"}} key={product.id}>
+              <div
+                className="col-9 border border-dark m-1"
+                style={{ width: "18rem" }}
+                key={product.id}
+              >
                 <h6>{product.title}</h6>
-                <img src={product.image} style={{width: "10rem", height:"12rem"}}/>
+                <img
+                  src={product.image}
+                  style={{ width: "10rem", height: "12rem" }}
+                />
                 <p>{`$ ${product.price}`}</p>
                 <small className="bg-info p-1">{`Rate: ${product.rating.rate}`}</small>
               </div>
