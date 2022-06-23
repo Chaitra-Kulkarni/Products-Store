c72254e6f8fffaa1106095aea3e770e015450afb
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-06-23 17:08:57 +0530

    first commit

---
+++ src/components/GetProducts.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const GetProducts = () => {
+
+  let API_URL =
+    `https://fakestoreapi.com/products`;
+
+    return (
+        <div>hi</div>
+    )
+}
+
+export default GetProducts;
