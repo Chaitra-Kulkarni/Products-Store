80886fbc971c9d9f8ca43268dbfd2e5185fa8f93
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-06-23 19:29:06 +0530

    minor: product UI and add button

--- src/components/GetProducts.jsx
+++ src/components/GetProducts.jsx
@@ -2,15 +2,28 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const GetProducts = () => {
+
   const [productsList, setProductsList] = useState([]);
+  const [category, setCategory] = useState('');
 
   const PRODUCTS_URL = "https://fakestoreapi.com/products";
+  const PRODUCTS_CATEGORY_URL = `https://fakestoreapi.com/products/category/${category}`;
+
   const loadProducts = () => {
     axios.get(PRODUCTS_URL).then((response) => {
-      console.log(response.data);
+    //   console.log(response.data);
       setProductsList(response.data);
     });
   };
+
+    useEffect(() => {
+        axios.get(PRODUCTS_CATEGORY_URL)
+        .then(response => {
+            console.log(response.data);
+        })
+    }, [category])
+  
+
   return (
     <>
       <button
@@ -19,35 +32,37 @@ const GetProducts = () => {
       >
         View Products
       </button>
+
       <div>
         <label>Filter :</label>
-        <a className="btn btn-warning btn-sm m-2" href="#" value="electronics">
+        <button onClick={() => setCategory('electronics')} className="btn btn-warning btn-sm m-2" href="#" value="electronics">
           Electronics
-        </a>
-        <a className="btn btn-warning btn-sm m-2" href="#" value="jewelery">
+        </button>
+        <button onClick={() => setCategory('jewelery')} className="btn btn-warning btn-sm m-2" href="#" value="jewelery">
           Jewelery
-        </a>
-        <a
+        </button>
+        <button onClick={() => setCategory(`men's clothing`)}
           className="btn btn-warning btn-sm m-2"
           href="#"
           value="men's clothing"
         >
           Men's clothing
-        </a>
-        <a
+        </button>
+        <button onClick={() => setCategory(`women's clothing`)}
           className="btn btn-warning btn-sm m-2"
           href="#"
           value="women's clothing"
         >
           Women's clothing
-        </a>
+        </button>
       </div>
+
       <div className="container-fluid">
         <div className="row">
           {productsList.map((product) => {
             return (
               <div
-                className="col-9 border border-dark m-1"
+                className="col-9 bg-success bg-opacity-25 border border-dark m-1"
                 style={{ width: "18rem" }}
                 key={product.id}
               >
@@ -56,8 +71,9 @@ const GetProducts = () => {
                   src={product.image}
                   style={{ width: "10rem", height: "12rem" }}
                 />
-                <p>{`$ ${product.price}`}</p>
+                <p style={{fontWeight: "bold"}}>{`$ ${product.price}`}</p>
                 <small className="bg-info p-1">{`Rate: ${product.rating.rate}`}</small>
+                <button className="btn btn-success m-2">Add</button>
               </div>
             );
           })}
